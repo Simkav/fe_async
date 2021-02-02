58310bc622fb13ec6c43c84d8cbfd18a1d658fa9
Simkav <Simkav2411@gmail.com>
2021-02-02 20:47:30 +0200

    Timer task

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,33 @@
 'use strict';
+// let i = 1;
+// const timer = function () {};
+// const id = setInterval(timer, 100);
+// console.time('1');
+const createTimer = function (from = 1, to = 20, interval = 100) {
+  if (from > to) {
+    throw new RangeError('From must be loweer than to');
+  }
+  console.time('1');
+  let i = from;
+  const id = setInterval(() => {
+    console.log(i++);
+    if (i > to) {
+      clearInterval(id);
+      console.timeEnd('1');
+    }
+  }, interval);
+};
 
+console.time('1');
+count();
 
+function count() {
+  let i = 1;
+  const id = setInterval(() => {
+    console.log(i++);
+    if (i > 20) {
+      clearInterval(id);
+      console.timeEnd('1');
+    }
+  }, 100);
+}
